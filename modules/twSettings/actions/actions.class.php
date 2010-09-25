e4a68a0dfa9719144f06e32debf8a9c2fa42ee83
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2010-09-25 20:54:02 +0200

    bugfixes for settings

--- modules/twSettings/actions/actions.class.php
+++ modules/twSettings/actions/actions.class.php
@@ -9,8 +9,13 @@ require_once dirname(__FILE__).'/../lib/twSettingsGeneratorHelper.class.php';
  * @package    twCore
  * @subpackage twSettings
  * @author     Your name here
- * @version    SVN: $Id: actions.class.php 2949 2009-11-07 20:35:23Z ldath $
+ * @version    SVN: $Id: actions.class.php 3346 2010-09-25 16:58:28Z ldath $
  */
 class twSettingsActions extends autoTwSettingsActions
 {
+	public function preExecute() {
+		sfConfig::set('tw_admin:default:module', 'administration');
+		sfConfig::set('tw_admin:default:category', 'settings');
+		return parent::preExecute();
+	}
 }
