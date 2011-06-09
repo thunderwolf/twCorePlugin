ccce14ef2f3266f37476dce19d520e99b9884ea4
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2011-06-10 01:18:24 +0200

    adding new table to core

---
+++ lib/form/twUserForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * twUser form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ */
+class twUserForm extends BasetwUserForm
+{
+  public function configure()
+  {
+  }
+}
